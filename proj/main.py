4291cd4ab47e98dc570427f177e5e80573990cc9
h3x4d3cim4l <przeglowa@gmail.com>
2022-12-12 22:46:21 +0100

    dodano obsluge 'waitress', poprawiono plik requirements

--- proj/main.py
+++ proj/main.py
@@ -19,3 +19,8 @@ def bytext():
 @app.route("/byname/")
 def byname():
     return render_template("byname.html")
+
+
+if __name__ == "__main__":
+    from waitress import serve
+    serve(app, host='127.0.0.1', port=5000)
